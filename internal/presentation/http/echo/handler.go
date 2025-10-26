50da1a55e5d2490b84e4859a653a4437cf9937c0
RyusukeYashiro <ryusukeyasiro@gmail.com>
2025-10-26 16:06:55 +0900

    fix: buildエラーの修正

--- internal/presentation/http/echo/handler.go
+++ internal/presentation/http/echo/handler.go
@@ -7,11 +7,11 @@ type Handler struct{}
 func NewHandler() *Handler { return &Handler{} }
 
 // ダミー実装（まずは５０１を返して起動確認）
-func (h *Handler) Login(c echo.Context) error                                  { return c.NoContent(501) }
-func (h *Handler) Logout(c echo.Context) error                                 { return c.NoContent(501) }
-func (h *Handler) Signup(c echo.Context) error                                 { return c.NoContent(501) }
-func (h *Handler) ListTasks(c echo.Context, _ ListTasksParams) error           { return c.NoContent(501) }
-func (h *Handler) CreateTask(c echo.Context) error                             { return c.NoContent(501) }
-func (h *Handler) DeleteTask(c echo.Context, _ int64) error                    { return c.NoContent(501) }
-func (h *Handler) GetTask(c echo.Context, _ int64) error                       { return c.NoContent(501) }
-func (h *Handler) UpdateTask(c echo.Context, _ int64) error                    { return c.NoContent(501) }
+func (h *Handler) Login(c echo.Context) error            { return c.NoContent(501) }
+func (h *Handler) Logout(c echo.Context) error           { return c.NoContent(501) }
+func (h *Handler) Signup(c echo.Context) error           { return c.NoContent(501) }
+func (h *Handler) ListTasks(c echo.Context) error        { return c.NoContent(501) }
+func (h *Handler) CreateTask(c echo.Context) error       { return c.NoContent(501) }
+func (h *Handler) DeleteTask(c echo.Context, _ int64) error { return c.NoContent(501) }
+func (h *Handler) GetTask(c echo.Context, _ int64) error { return c.NoContent(501) }
+func (h *Handler) UpdateTask(c echo.Context, _ int64) error { return c.NoContent(501) }
