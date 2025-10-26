90fe19dcecd89a817332442cd7cd8ff20e2725e0
RyusukeYashiro <ryusukeyasiro@gmail.com>
2025-10-27 02:19:57 +0900

    feat: clockの実装

--- internal/infrastructure/clock/clock.go
+++ internal/infrastructure/clock/clock.go
@@ -1,3 +1,18 @@
 package clock
 
-// TODO: implement
+import (
+	"time"
+
+	"github.com/ryusuke/task_app_layerx/internal/domain"
+)
+
+// RealClock は domain.Clock の本番実装です
+type RealClock struct{}
+
+func New() domain.Clock {
+	return &RealClock{}
+}
+
+func (c *RealClock) Now() time.Time {
+	return time.Now()
+}
